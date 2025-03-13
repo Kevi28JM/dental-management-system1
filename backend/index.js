3e4c7f2084e5a4d65f893a3f2747a4493297701e
Kevi28JM <fernandogimhani165@gmail.com>
2025-03-13 23:54:27 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ const db = require("./models/db");
 dotenv.config();
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Default Route
 app.get("/", (req, res) => {
     res.send("Backend is running...");
 });
@@ -18,6 +21,7 @@ const userRoutes = require("./routes/userRoutes");
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
