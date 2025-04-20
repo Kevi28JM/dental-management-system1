c0875fb803c1a8e77a872eb1744a1cbbae23c7d9
Kevi28JM <fernandogimhani165@gmail.com>
2025-04-20 19:24:07 +0530

    update the bookPage.js and css files

--- frontend/src/app/bookPage/page.js
+++ frontend/src/app/bookPage/page.js
@@ -1,8 +1,9 @@
 import BookAppointment from "@/components/BookAppointment";
+import "@/styles/bookPage.css";
 
 function bookPage() {
   return (
-    <div>
+    <div className="book-container">
       <h1>Book an Appointment</h1>
       <BookAppointment />
     </div>
