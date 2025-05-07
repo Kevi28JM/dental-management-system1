b295537659a85c9a0f4d7eba50dd05fbd064cf35
Kevi28JM <fernandogimhani165@gmail.com>
2025-05-07 22:19:46 +0530

    add patientSidebar

---
+++ frontend/src/components/PatientSidebar.js
@@ -0,0 +1,49 @@
+"use client"; // Ensure this is a client-side component
+
+import Link from "next/link";
+import { usePathname } from "next/navigation"; // Detects current page
+import { useEffect } from "react";
+import "../styles/PatientSidebar.css"; // Import the CSS file (make sure it's a global stylesheet)
+
+const PatientSidebar = () => {
+  const pathname = usePathname(); // Get current route
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <span className="sidebar-title">Family Dental Surgery</span>
+        <img src="/logo.png" alt="Dental Logo" className="sidebar-logo" />
+      </div> 
+
+
+
+      {/* Sidebar Menu */}
+      <ul className="sidebar-menu">
+          <li className={pathname === "/PatientDashboard" ? "active" : ""}>
+            <Link href="/PatientDashboard">
+            <img src="/dashboard_icon.png" alt="dashboard icon" className="dashboard-logo"/>  Dashboard</Link>
+          </li>
+
+           
+
+          <li className={pathname === "/MyAppointments" ? "active" : ""}>
+            <Link href="/MyAppointments">     
+            <img src="/appointment_icon.png" alt="appointment icon" className="appointment-logo"/>  Appointments</Link>
+          </li>
+
+          <li className={pathname === "/DentistList" ? "active" : ""}>
+            <Link href="/DentistList">    
+            <img src="/dashboard_icon.png" alt="dashboard icon" className="dashboard-logo"/>  Dentist Availability</Link>
+          </li>
+
+         {/*} <li className={pathname === "/patients" ? "active" : ""}>
+            <Link href="/patient-register">
+            <img src="/patient_icon.png" alt="patient icon" className="patient-logo"/>  Patient Register</Link>
+          </li>
+          */}
+        </ul>
+    </aside>
+  );
+}
+
+export default PatientSidebar;
